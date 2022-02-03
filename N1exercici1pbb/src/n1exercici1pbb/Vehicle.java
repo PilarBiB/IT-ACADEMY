785033bb80d5cd01e04d057af327a627bf9caa83
pilar <pilar@192.168.1.39>
2022-02-03 10:59:21 +0100

    Versió entregada

---
+++ N1exercici1pbb/src/n1exercici1pbb/Vehicle.java
@@ -0,0 +1,19 @@
+package n1exercici1pbb;
+
+public class Vehicle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Vehicle cotxe1 = new Vehicle();
+	
+	cotxe1.iniciar();
+	                          
+	}
+	public Vehicle () {
+		System.out.println("Soc el constructor");
+	}
+	public void  iniciar( ) {
+			System.out.println("soc al metode iniciar");
+	}
+}
