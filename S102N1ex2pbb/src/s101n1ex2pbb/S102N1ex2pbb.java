fb5b4be4db22192d4557b9b5b251142673fd0722
pilar <pilar@192.168.1.39>
2022-02-03 11:20:23 +0100

    Versió entregada

---
+++ S102N1ex2pbb/src/s101n1ex2pbb/S102N1ex2pbb.java
@@ -0,0 +1,17 @@
+package s101n1ex2pbb;
+
+public class S102N1ex2pbb {
+
+	
+	public static void main(String[] args) {
+		try {
+			String e = null;
+		
+		e.length();  
+			
+		}catch (Exception e) {
+		System.out.println("Exception java.lang.NullPointerException");
+		}
+	}
+}
+
